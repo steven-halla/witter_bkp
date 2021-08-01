1dbc3c8fb07393a0f8f311b7614932664471fddd
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-01 16:42:56 -0700

    "more work on new tweet screen"

--- screens/NewTweetScreen.tsx
+++ screens/NewTweetScreen.tsx
@@ -1,23 +1,84 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { AntDesign, EvilIcons } from "@expo/vector-icons";
+import Colors from "../constants/Colors";
+import ProfilePicture from "../components/ProfilePicture";
 
 
 
 export default function NewTweetScreen() {
+
+  const onPostTweet = () => {
+    console.warn(  "onPostTweet");
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <View style={styles.headerContainer}>
+          <AntDesign name="close" size={30} color={Colors.light.tint} />
+          <TouchableOpacity style={styles.button} onPress={onPostTweet}>
+            <Text style={styles.buttonText}>Tweet</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.newTweetContainer}>
+            <ProfilePicture image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuqI35mONQc0brx4iHoiHSW5J0ZTHnn0fOvw&usqp=CAU'}/>
+            <View style={styles.inputsContainer}>
+              <TextInput
+                numberOfLines={3}
+                style={styles.tweetInput}
+                placeholder={"What's happening?"}
+              />
+              <TextInput
+                style={styles.imageInput}
+                placeholder={"image url(optional)"}
+              />
+          </View>
+        </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: 'white',
+  },
+  headerContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 15,
+
+  },
+  button: {
+    backgroundColor: Colors.light.tint,
+    borderRadius: 30,
+  },
+  buttonText: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  newTweetContainer: {
+    flexDirection: 'row',
+    padding: 15,
+
+  },
+  inputsContainer: {
+    marginLeft: 10,
+
+  },
+  tweetInput: {
+    height: 100,
+    maxHeight: 300,
+  },
+  imageInput: {
+
   },
 });
